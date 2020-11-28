9a1d0632443eacdd8c465d80e379067451d12424
mow09 <donj31313@gmail.com>
2020-11-28 23:58:46 +0100

    python3.8

--- spirit/spirit/settings.py
+++ spirit/spirit/settings.py
@@ -83,7 +83,7 @@ if 'TESTDUDE' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': str(BASE_DIR / 'db.sqlite3'),
         }
     }
 elif 'MACOS' in os.environ:
